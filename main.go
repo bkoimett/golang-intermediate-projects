d0e78f1c20fcadc3e51daac77b507543d6f9126e
bkoimett <benkoimet@gmail.com>
2025-06-04 12:12:02 +0300

    feat (main) : create two structs to be used(director and movie) and declare a global variable (slice)called movies

--- main.go
+++ main.go
@@ -11,6 +11,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Movie struct {
+	ID string `json:"id"`
+	Isbn string `json:"id"`
+	Title string `json:"id"`
+	Director *Director
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
 func main(){
-	
+
 }
