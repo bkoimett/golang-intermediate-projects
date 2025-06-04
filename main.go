574c2d2994f9c9f1434bf42d77bda45cb695fd06
bkoimett <benkoimet@gmail.com>
2025-06-04 12:19:45 +0300

    chore (main) : create a boiler plate for how routes shall be handled and start the server

--- main.go
+++ main.go
@@ -26,5 +26,15 @@ type Director struct {
 var movies []Movie
 
 func main(){
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("server has started on https://localhost:8000 ...")
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
