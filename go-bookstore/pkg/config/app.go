5f1417e0159615f79b698a469761ff1efa837180
bkoimett <benkoimet@gmail.com>
2025-06-06 20:54:58 +0300

    feat (app.go) : create dababase connection

---
+++ go-bookstore/pkg/config/app.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+// enables interactiom with db
+var (
+	db = gorm.DB
+)
+
+// opens connection to db
+func Connect(){
+	d, err := gorm.Open("mysql", "bkoimett:08062016Vanna#/simplerest?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	db = d
+}
+
+// used everywhere else
+func GetDB() *gorm.DB{
+	return db
+}
