152080a145260cf9bfec26659542cf16cc9d26a2
bkoimett <benkoimet@gmail.com>
2025-06-07 10:24:12 +0300

    feat (models): create models with struct for data and create a db connections from our config

--- go-bookstore/pkg/models/book.go
+++ go-bookstore/pkg/models/book.go
@@ -9,8 +9,13 @@ var db *gorm.DB
 
 type Book struct{
 	gorm.Model
-	Name string `gorm:""json:"name"`
+	Name string `gorm:"" json:"name"`
 	Author string `json:"author"`
 	Publication string `json:"publication"`
 }
 
+func init(){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
