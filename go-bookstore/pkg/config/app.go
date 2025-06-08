1a2921d259fdbe0246f77f6e821cd343229d44e9
bkoimett <benkoimet@gmail.com>
2025-06-08 16:53:02 +0300

    MVP: final commit - tested endpoints using postman - started mysql in a containerised docker

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -12,7 +12,7 @@ var (
 
 // opens connection to db
 func Connect(){
-	d, err := gorm.Open("mysql", "bkoimett:08062016Vanna#/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "bkoimett:bwanaChairman@tcp(127.0.0.1:3307)/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
